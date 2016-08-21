550e4faabc00960793800ca0aa7279a1d1c6fd62
Ben Poor <ben.poor@thisisglobal.com>
2016-08-21 20:15:55 +0100

    add files for pypi

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from distutils.core import setup
 
 setup(name='python-hybridspi',
       version='1.0.0',
-      description='Hybrid Radio SPI implementation',
+      description='Hybrid Radio SPI implementation (ETSI TS 102 818 v3.1.1, ETSI TS 102 371 v.3.1.1)',
       author='Ben Poor',
       author_email='magicbadger@gmail.com',
-      packages=['spi', 'spi.xml'],
+      url='https://github.com/magicbadger/python-hybridspi',
+      download_url='',
+      keywords=['dab', 'spi', 'hybrid', 'radio'],
+      packages=['spi', 'spi.xml', 'spi.binary'],
       package_dir = {'' : 'src'}
 )
