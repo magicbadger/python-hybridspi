95a80171a695b1e2d7910c9f9bf0012185780f0a
Ben Poor <poor@ebu.ch>
2021-04-16 15:56:41 +0200

    updated version and tarball to 0.3.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 
 setup(name='hybridspi',
-      version='0.2.0',
-      description='Hybrid Radio SPI implementation (ETSI TS 102 818 v3.1.1, ETSI TS 102 371 v.3.1.1)',
+      version='0.3.0',
+      description='Hybrid Radio SPI implementation (ETSI TS 102 818, ETSI TS 102 371)',
       author='Ben Poor',
       author_email='magicbadger@gmail.com',
       url='https://github.com/magicbadger/python-hybridspi',
       license='GPL2',
-      download_url='https://github.com/magicbadger/python-hybridspi/tarball/0.2.0',
+      download_url='https://github.com/magicbadger/python-hybridspi/tarball/0.3.0',
       keywords=['dab', 'spi', 'hybrid', 'radio'],
       packages=['spi', 'spi.xml', 'spi.binary'],
       package_dir = {'' : 'src'},
